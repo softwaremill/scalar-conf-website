adbaa367243397d8aeb56e37472f8cc9f6c69619
Krzysztof Grajek <krzysztof.grajek@vocado.com>
2013-10-09 09:10:56 +0200

    fixed countdown

--- countdown/js/ext.js
+++ countdown/js/ext.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-	$('#countdown').countdown( {date: '5 april 2014 09:00:00'} );
+	$('#countdown').countdown( {date: '5 april 2014 09:00:00',format: 'ddd:hh:mm:ss'} );
 });
