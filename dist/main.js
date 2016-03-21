0321a014022fadb9fff3ef9ee19a8a03b5852fce
marta <marta.weronika.smektala@gmai.com>
2016-03-21 12:38:03 +0100

    update hotels, without link to homepage

--- dist/main.js
+++ dist/main.js
@@ -1,14 +1,14 @@
 function initialize() {
   var centerLatlng = new google.maps.LatLng(52.221421, 21.004352);
-  var libraryLatlng = new google.maps.LatLng(52.214325, 21.002829);
-  var MDMLatlng = new google.maps.LatLng(52.221312, 21.016294);
-  var ReytanLatlng = new google.maps.LatLng(52.208000, 21.020285);
-  var IbisLatlng = new google.maps.LatLng(52.254103, 20.998860);
+  var ConfLatlng = new google.maps.LatLng(52.249499, 20.993205);
+  var RadissonLatlng = new google.maps.LatLng(52.236786, 20.998371);
+  var IbisCetrumLatlng = new google.maps.LatLng(52.208000, 21.020285);
+  var IbisLatlng = new google.maps.LatLng(52.254208,  20.998931);
   var RialtoLatlng = new google.maps.LatLng(52.223247, 21.008117);
 
 
   var mapOptions = {
-    zoom: 13,
+    zoom: 11,
     center: centerLatlng,
     disableDefaultUI: true
   };
@@ -28,7 +28,7 @@ function initialize() {
   strokeWeight: 2
   };
 
-  var libraryMarker = {
+  var ConfMarker = {
   path: 'M13,1 C6.4,1,1,6.1,1,12.5S13,33,13,33s12-14.2,12-20.5S19.6,1,13,1z',
   fillColor: '#114476',
   fillOpacity: 1,
@@ -39,59 +39,59 @@ function initialize() {
 
 // ============================================
 
-  var libraryWindow = new google.maps.InfoWindow({
-  content: '<div class="info-popup"><h6>Biblioteka Narodowa</h6><p>(Scalar conference venue)</p></div>'
+  var ConfWindow = new google.maps.InfoWindow({
+  content: '<div class="info-popup"><h6>Centrum Konferencyjne Muranow</h6><p>Scalar conference venue</p><img src="images/museum_photoXS.png"/></div>'
   });
 
-  var libraryMarker = new google.maps.Marker({
-  position: libraryLatlng,
+  var ConfMarker = new google.maps.Marker({
+  position: ConfLatlng,
   map: map,
-  icon: libraryMarker,
-  title: 'Biblioteka Narodowa - Conference venue'
+  icon: ConfMarker,
+  title: 'Centrum Konferencyjne Muranow - Conference venue'
   });
 
-  google.maps.event.addListener(libraryMarker, 'click', function() {
-  libraryWindow.open(map, libraryMarker);
+  google.maps.event.addListener(ConfMarker, 'click', function() {
+ ConfWindow.open(map, ConfMarker);
   });
 
 // ============================================
 
-  var mdmWindow = new google.maps.InfoWindow({
-  content: '<div class="info-popup"><a href="http://www.hotelmdm.com.pl/default-en.html"><h6>MDM Hotel<img src="images/icons/star.svg"/><img src="images/icons/star.svg"/><img src="images/icons/star.svg"/></h6></a><p>Very good location, close to the conference venue as well as places worth seeing in Warsaw.</p><p><b>Price:</b> 251.64 PLN (68 USD) for single or double room (<b><i>included:</i></b> 8% VAT, breakfast, internet)</p><p><b>Address:</b> pl. Konstytucji 1</p><p><b>Email:</b> <a href="mailto:hotel.mdml@syren.com.pl">hotel.mdml@syren.com.pl</a></p><p><b>Promo Code:</b> scalar 2015</p><p><a href="http://warszawa.jakdojade.pl/?fn=plac+Konstytucji+1&fc=52.22139:21.01635&tn=BIBLIOTEKA+NARODOWA&ts=BIBLIOTEKA+NARODOWA&tc=52.21311:21.00624&d=11.04.15&h=09:00&ia=false&t=0&n=0&ri=0&cid=3000&as=true&locale=en#tabId=0&">How to get to conference venue</a></p></div>'
+  var RadissonWindow = new google.maps.InfoWindow({
+  content: '<div class="info-popup"><a href="https://www.radissonblu.com/en#/map-results"><h6>Radisson Blu Centrum Hotel<br><img src="images/icons/star.svg"/><img src="images/icons/star.svg"/><img src="images/icons/star.svg"/><img src="images/icons/star.svg"/><img src="images/icons/star.svg"/></h6></a><p>Very close to the venue of the conference, planty of luxury in a very good price.</p><p><b>Price:</b> 312,12 PLN PLN for single &amp; 376,92 PLN for double room (<b><i>included:</i></b> 8% VAT, breakfast and Internet)</p><p><b>Address:</b> Grzybowska 24</p><p><b>Email:</b> <a href="mailto:reservations.warsaw@radissonblu.com">reservations.warsaw@radissonblu.com</a></p><p><b>Promo Code:</b> scalar 2016</p><p><a href="http://warszawa.jakdojade.pl/?fn=plac+Konstytucji+1&fc=52.22139:21.01635&tn=BIBLIOTEKA+NARODOWA&ts=BIBLIOTEKA+NARODOWA&tc=52.21311:21.00624&d=11.04.15&h=09:00&ia=false&t=0&n=0&ri=0&cid=3000&as=true&locale=en#tabId=0&">How to get to conference venue</a></p></div>'
   });
 
-  var mdmMarker = new google.maps.Marker({
-    position: MDMLatlng,
+  var RadissonMarker = new google.maps.Marker({
+    position: RadissonLatlng,
     map: map,
     icon: regularMarker,
-    title: 'MDM Hotel'
+    title: 'RasissonBlu Centrum Hotel'
   });
 
-  google.maps.event.addListener(mdmMarker, 'click', function() {
-  mdmWindow.open(map, mdmMarker);
+  google.maps.event.addListener(RadissonMarker, 'click', function() {
+  RadissonWindow.open(map, RadissonMarker);
   });
 
   // ============================================
 
-  var reytanWindow = new google.maps.InfoWindow({
-  content: '<div class="info-popup"><a href="http://www.reytan.pl/en/core/home.php?_PageID=39176"><h6>Reytan Hotel<img src="images/icons/star.svg"/><img src="images/icons/star.svg"/><img src="images/icons/star.svg"/></h6></a><p><b>Price:</b> 180 PLN (49 USD) for single room &amp; 240 PLN (65 USD) for double room (<b><i>included:</i></b> 8% VAT, breakfast)</p><p><b>Address:</b> ul. Tadeusza Rejtana 6</p><p><b>Email:</b> <a href="mailto:rez@reytan.pl">rez@reytan.pl</a></p><p><b>Promo Code:</b> scalar 2015</p><p><a href="https://www.google.pl/maps/dir/HOTEL+REYTAN,+Tadeusza+Rejtana,+Warsaw/Biblioteka+Narodowa,+aleja+Niepodleg%C5%82o%C5%9Bci,+Warsaw/@52.211123,21.0026936,15z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x471ecab7cbed96f5:0xb9663a16f2048772!2m2!1d21.020343!2d52.208003!1m5!1m1!1s0x471eccc3d4b32f9d:0x9140378313c7f946!2m2!1d21.003451!2d52.213658!3e3">How to get to conference venue</a></p></div>'
+  var IbisCetrumWindow = new google.maps.InfoWindow({
+  content: '<div class="info-popup"><a href="http://www.accorhotels.com/gb/hotel-2894-ibis-warszawa-centrum/index.shtml"><h6>IBIS Warszawa Centrum<img src="images/icons/star.svg"/><img src="images/icons/star.svg"/></h6></a><p></p><p><b>Price:</b> 165 PLN for single room &amp; 185 PLN for double room (<b><i>included:</i></b> 8% VAT, breakfast and Internet)</p><p><b>Address:</b> Ave Solidarnosci 165</p><p><b>Email:</b> <a href="mailto:H2894@accor.com">H2894@accor.com</a></p><p><b>Promo Code:</b> scalar 2016</p><p><a href="https://www.google.pl/maps/dir/HOTEL+REYTAN,+Tadeusza+Rejtana,+Warsaw/Biblioteka+Narodowa,+aleja+Niepodleg%C5%82o%C5%9Bci,+Warsaw/@52.211123,21.0026936,15z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x471ecab7cbed96f5:0xb9663a16f2048772!2m2!1d21.020343!2d52.208003!1m5!1m1!1s0x471eccc3d4b32f9d:0x9140378313c7f946!2m2!1d21.003451!2d52.213658!3e3">How to get to conference venue</a></p></div>'
   });
 
-  var reytanMarker = new google.maps.Marker({
-    position: ReytanLatlng,
+  var IbisCetrumMarker = new google.maps.Marker({
+    position: IbisCetrumLatlng,
     map: map,
     icon: regularMarker,
-    title: 'Reytan Hotel'
+    title: 'IBIS Warszawa Centrum'
   });
 
-  google.maps.event.addListener(reytanMarker, 'click', function() {
-  reytanWindow.open(map, reytanMarker);
+  google.maps.event.addListener(IbisCetrumMarker, 'click', function() {
+  IbisCetrumWindow.open(map, IbisCetrumMarker);
   });
 
   // ============================================
 
   var ibisWindow = new google.maps.InfoWindow({
-  content: '<div class="info-popup"><a href="http://www.ibis.com/gb/hotel-3714-ibis-warszawa-stare-miasto-old-town/index.shtml"><h6>Ibis Hotel<img src="images/icons/star.svg"/><img src="images/icons/star.svg"/><img src="images/icons/star.svg"/></h6></a><p>Located very close to the Old Town.</p><p><b>Price:</b> 157.25 PLN (43 USD) for single or double room (<b><i>included:</i></b> 8% VAT), breakfast is 34 PLN per person</p><p><b>Address:</b> ul. Muranowska 2</p><p><b>Email:</b> <a href="mailto:h3714-re@accor.com">h3714-re@accor.com</a></p><p><b>Promo Code:</b> scalar 2015</p><p><a href="http://warszawa.jakdojade.pl/index.html?fn=Muranowska+2&fc=52.25409:20.99863&tn=BIBLIOTEKA+NARODOWA&ts=BIBLIOTEKA+NARODOWA&tc=52.21311:21.00624&d=11.04.15&h=09:00&ia=false&t=0&n=0&ri=0&cid=3000&as=true#tabId=0&">How to get to conference venue</a></p></div>'
+  content: '<div class="info-popup"><a href="http://www.accorhotels.com/gb/hotel-3714-ibis-warszawa-stare-miasto-old-town/index.shtml"><h6>Ibis Hotel<img src="images/icons/star.svg"/><img src="images/icons/star.svg"/><img src="images/icons/star.svg"/></h6></a><p>Located very close to the Old Town.</p><p><b>Price:</b> 165 PLN for single &amp; 185 PLN for double room (<b><i>included:</i></b> 8% VAT, breakfast and Internet)</p><p><b>Address:</b> ul. Muranowska 2</p><p><b>Email:</b> <a href="mailto:h3714-re@accor.com">h3714-re@accor.com</a></p><p><b>Promo Code:</b> scalar 2016</p> <p><a href="http://warszawa.jakdojade.pl/index.html?fn=Muranowska+2&fc=52.25409:20.99863&tn=BIBLIOTEKA+NARODOWA&ts=BIBLIOTEKA+NARODOWA&tc=52.21311:21.00624&d=11.04.15&h=09:00&ia=false&t=0&n=0&ri=0&cid=3000&as=true#tabId=0&">How to get to conference venue</a></p></div>'
   });
 
   var ibisMarker = new google.maps.Marker({
@@ -106,7 +106,7 @@ function initialize() {
   });
 
   // ============================================
-
+/*
   var rialtoWindow = new google.maps.InfoWindow({
   content: '<div class="info-popup"><a href="http://www.rialto.pl/en/"><h6>Rialto Hotel<img src="images/icons/star.svg"/><img src="images/icons/star.svg"/><img src="images/icons/star.svg"/><img src="images/icons/star.svg"/><img src="images/icons/star.svg"/></h6></a><p>A bit of luxury in the first boutique hotel in Warsaw.</p><p><b>Price:</b> 360 PLN (97 USD) for single room &amp; 460 PLN (124 USD) for double room (<b><i>included:</i></b> 8% VAT, breakfast, internet, sauna, gym)</p><p class="address"><b>Address:</b> ul. Wilcza 73</p><p><b>Email:</b> <a href="mailto:reservations@rialto.pl">reservations@rialto.pl</a></p><p><b>Promo Code:</b> scalar 2015</p><p><a href="https://www.google.pl/maps/dir/Hotel+Rialto,+Wilcza+73,+00-670+Warszawa/Biblioteka+Narodowa,+aleja+Niepodleg%C5%82o%C5%9Bci,+Warsaw/@52.218091,20.9972494,15z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x471eccec43b7a925:0xc3ff25a2c4c6653e!2m2!1d21.008214!2d52.22327!1m5!1m1!1s0x471eccc3d4b32f9d:0x9140378313c7f946!2m2!1d21.003451!2d52.213658!3e3">How to get to conference venue</a></p></div>'
   });
@@ -121,7 +121,7 @@ function initialize() {
   google.maps.event.addListener(rialtoMarker, 'click', function() {
   rialtoWindow.open(map, rialtoMarker);
   });
-
+*/
 }
 
 var styles = [
