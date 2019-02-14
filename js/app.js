d5397c0e65651009652079ae83a7e5148e647fd7
Tomasz Krawczyk <grubach1@gmail.com>
2019-02-14 18:18:18 +0100

    floating scalars

--- js/app.js
+++ js/app.js
@@ -9,33 +9,60 @@ const getDocHeight = () =>
   );
 
 $(() => {
+  let tick = 0;
+
   const bubblesElement = document.getElementById("bubbles");
+  const scalarElement = document.getElementById("scalar");
   const bottomElement = document.getElementById("bottom");
+  const lightElement = document.getElementById("light");
+
   let docHeight = getDocHeight();
   let { pageYOffset, innerHeight } = window;
 
   const scrollBubbles = () => {
-    bubblesElement.style.transform = `translateY(${-pageYOffset / 3}px`;
+    bubblesElement.style.transform = `translateY(${-pageYOffset / 4}px)`;
+  };
+
+  const scrollScalar = () => {
+    const cycle = (tick / 2) % 1345
+    const sinusoid = Math.sin(cycle * Math.PI / 1345 * 2) * 50
+    scalarElement.style.transform = `translate(${-1 * cycle}px, ${-pageYOffset / 3 + sinusoid}px)`;
+  };
+
+  const scrollLight = () => {
+    lightElement.style.transform = `translateY(${-pageYOffset / 20}px)`;
   };
 
   const scrollBottom = () => {
-    bottomElement.style.transform = `translateY(${(docHeight - pageYOffset - innerHeight) / 30}px`;
+    bottomElement.style.transform = `translateY(${(docHeight -
+      pageYOffset -
+      innerHeight) /
+      30}px)`;
   };
 
   const handleScroll = () => {
     pageYOffset = window.pageYOffset;
     innerHeight = window.innerHeight;
-    scrollBubbles()
-    scrollBottom()
-  }
+    scrollBubbles();
+    scrollScalar();
+    scrollBottom();
+    scrollLight();
+  };
+
+  const handleFrame = () => {
+    tick++;
+    scrollScalar();
+  };
 
   const setBubbles = () => {
     docHeight = getDocHeight();
-    bubblesElement.style.height = `${docHeight / 3}px`;
+    bubblesElement.style.height = `${docHeight}px`;
+    scalarElement.style.height = `${docHeight}px`;
     handleScroll();
   };
 
   setBubbles();
+  setInterval(handleFrame, 1000 / 40);
   window.addEventListener("scroll", handleScroll, false);
   window.addEventListener("resize", setBubbles);
 });
