1409c2aea7daa679d2e1b3dab6e49ebfdd17aa58
Tomasz Krawczyk <grubach1@gmail.com>
2019-02-14 15:54:40 +0100

    bottom paralax

--- js/app.js
+++ js/app.js
@@ -10,17 +10,32 @@ const getDocHeight = () =>
 
 $(() => {
   const bubblesElement = document.getElementById("bubbles");
+  const bottomElement = document.getElementById("bottom");
+  let docHeight = getDocHeight();
+  let { pageYOffset, innerHeight } = window;
 
   const scrollBubbles = () => {
-    const { pageYOffset } = window;
     bubblesElement.style.transform = `translateY(${-pageYOffset / 3}px`;
   };
 
+  const scrollBottom = () => {
+    bottomElement.style.transform = `translateY(${(docHeight - pageYOffset - innerHeight) / 30}px`;
+  };
+
+  const handleScroll = () => {
+    pageYOffset = window.pageYOffset;
+    innerHeight = window.innerHeight;
+    scrollBubbles()
+    scrollBottom()
+  }
+
   const setBubbles = () => {
-    bubblesElement.style.height = `${getDocHeight() / 3}px`;
-    scrollBubbles();
+    docHeight = getDocHeight();
+    bubblesElement.style.height = `${docHeight / 3}px`;
+    handleScroll();
   };
 
   setBubbles();
-  window.addEventListener("scroll", scrollBubbles, false);
+  window.addEventListener("scroll", handleScroll, false);
+  window.addEventListener("resize", setBubbles);
 });
