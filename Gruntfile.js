f906fd6d477b21af8fcd851663e7e9bfe91ba6b6
idabzo <ida.bzowska@gmail.com>
2020-03-23 22:30:06 +0100

    main changed

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,8 @@ module.exports = function(grunt) {
           'dist/blog.html': ['pug/blog.pug'],
           'dist/hotels.html': ['pug/hotels.pug'],
           'dist/support.html': ['pug/support.pug'],
-          'dist/events-2020.html': ['pug/events-2020.pug']
+          'dist/events-2020.html': ['pug/events-2020.pug'],
+          'dist/2020-workshop.html': ['pug/2020-workshop.pug']
         },
         filters: {
           marked: marked
